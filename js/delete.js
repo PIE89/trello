36d62624977710486d6fd97da4535d044ed4e2f8
PIE89 <spyder34@yandex.ru>
2024-10-04 13:46:09 +0300

    bug fixed

--- js/delete.js
+++ js/delete.js
@@ -1,5 +1,10 @@
 const btnDelete = document.querySelector(".delete");
 
+btnDelete.addEventListener("dragover", function (e) {
+  console.log("dragover");
+  e.preventDefault(); 
+});
+
 btnDelete.addEventListener("drop", function (e) {
-  console.log("drop"); // Не работает, так как элемент btnDelete спозиционирован абсолютно
+  console.log("drop");
 });
